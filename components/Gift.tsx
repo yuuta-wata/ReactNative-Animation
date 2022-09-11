5ba59d6fa1a5a03405eb812df22b99c7071af63d
Yuuta Watanabe <yuutakkuma.74@gmail.com>
2022-09-12 00:12:06 +0900

    adjustment animation

--- components/Gift.tsx
+++ components/Gift.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Button } from "react-native";
 import Animated, {
   useSharedValue,
-  withSpring,
   withTiming,
   withRepeat,
   withSequence,
@@ -13,32 +12,33 @@ import { Svg, Path } from "react-native-svg";
 const AnimatedPath = Animated.createAnimatedComponent(Path);
 
 export function Gift() {
-  const leftQX = useSharedValue(30);
-  const rightQX = useSharedValue(170);
+  const boxTopX = useSharedValue(30);
   const startX = 30;
   const startY = startX + 50;
   const endPositionY = startY + 100;
   const bottomSideEndPositionX = 170;
+  const leftQX = useSharedValue(startX);
+  const rightQX = useSharedValue(bottomSideEndPositionX);
 
   const strokeWidth = 8;
 
   const animatedProps = useAnimatedProps(() => {
     // 左辺
     const leftSideStart = `M ${startX} ${startY}`;
-    const leftSideQuadraticBezierCurve = `Q ${leftQX.value} ${130}`;
+    const leftSideQuadraticBezierCurve = `Q ${leftQX.value} ${150}`;
     const leftSideEnd = ` ${startX} ${endPositionY} `;
     const leftSide = `${leftSideStart} ${leftSideQuadraticBezierCurve}, ${leftSideEnd}`;
     // 下辺
     const bottomSide = `H ${bottomSideEndPositionX}`;
     // 右辺
     const rightSideStart = `M ${bottomSideEndPositionX} ${endPositionY}`;
-    const rightSideQuadraticBezierCurve = `Q ${rightQX.value} ${130}`;
+    const rightSideQuadraticBezierCurve = `Q ${rightQX.value} ${150}`;
     const rightSideEnd = `${bottomSideEndPositionX} ${startY}`;
     const rightSide = `${rightSideStart}, ${rightSideQuadraticBezierCurve} ${rightSideEnd}`;
     // 上辺
     const topSide = `H ${startX}`;
 
-    // box path
+    // box path 左辺から反時計回り
     const d = `${leftSide}, ${bottomSide} ${rightSide} ${topSide}`;
     return { d };
   });
@@ -58,13 +58,17 @@ export function Gift() {
       <Button
         title="Move"
         onPress={() => {
+          const initLeft = 30;
+          const initRight = 170;
           leftQX.value = withSequence(
-            withTiming(5, { duration: 1000 }),
-            withRepeat(withTiming(30, { duration: 100 }), 5, true)
+            withTiming(initLeft - 25, { duration: 1000 }),
+            withRepeat(withTiming(initLeft - 15, { duration: 100 }), 5, true),
+            withTiming(initLeft, { duration: 100 })
           );
           rightQX.value = withSequence(
-            withTiming(170 + 25, { duration: 1000 }),
-            withRepeat(withTiming(170, { duration: 100 }), 5, true)
+            withTiming(initRight + 25, { duration: 1000 }),
+            withRepeat(withTiming(initRight + 15, { duration: 100 }), 5, true),
+            withTiming(initRight, { duration: 100 })
           );
         }}
       />
